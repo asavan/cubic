1dc36bddda05dfdda909c4550b1010087fdb8537
a.savanovich <asavanster@gmail.com>
2021-02-10 15:05:11 +0300

    fix cube render

--- main.js
+++ main.js
@@ -1,45 +1,9 @@
-
-
 /* Loop
 ---------------------------------------------------------------- */
 
-
-function renderFaces(faces, v, htmlV) {
-
-    // shedule the next update
-    // requestAnimationFrame(loop);
-
-    faces.forEach(function(face, i) {
-        // Extract the elements transform
-        const vertexData = computeVertexData(face);
-
-        // Draw the vertices
-        renderVertex(0, face, vertexData.a, i, v, htmlV);
-        renderVertex(1, face, vertexData.b, i, v, htmlV);
-        renderVertex(2, face, vertexData.c, i, v, htmlV);
-        renderVertex(3, face, vertexData.d, i, v, htmlV);
-    });
-}
-
-
-/* Renders a vertex to the DOM
----------------------------------------------------------------- */
-
-function faceToVertex(faceId, localV) {
-    console.log(faceId, localV);
-    const map = [[0, 1, 2, 3],
-        [3,2,6,7],
-        [4,5,6,7],
-        [0,1,4,5],
-        [5,1,2,6],
-        [0,3,4,7]
-    ];
-    return map[faceId][localV];
-}
-
 function renderVertex(id1, face, vertex, faceId, v, htmlV) {
     const id = faceToVertex(faceId, id1);
-    const    x = vertex.x.toFixed(2),
+    const x = vertex.x.toFixed(2),
         y = vertex.y.toFixed(2),
         z = vertex.z.toFixed(2),
         s = 1.5; // scale to keep the text readable
@@ -62,18 +26,56 @@ function renderVertex(id1, face, vertex, faceId, v, htmlV) {
         "transform: translate3d(" + x + "px," + y + "px," + z + "px) scale(" + s + ");";
 }
 
+function faceToVertex(faceId, localV) {
+    // console.log(faceId, localV);
+    const map = [
+        [0, 3, 7, 4],
+        [2, 1, 5, 6],
+        [1, 0, 4, 5],
+        [3, 2, 6, 7],
+        [4, 7, 6, 5],
+        [1, 2, 3, 0]
+    ];
+    return map[faceId][localV];
+}
+
+function renderFaces(faces, v, htmlV) {
+
+    // shedule the next update
+    // requestAnimationFrame(loop);
+
+    // let index = 0;
+    for (let i = 0; i < 6; ++i) {
+        // let i = index % 6;
+        const face = faces[i]
+        // Extract the elements transform
+        const vertexData = computeVertexData(face);
+        // Draw the vertices
+        renderVertex(0, face, vertexData.a, i, v, htmlV);
+        renderVertex(1, face, vertexData.b, i, v, htmlV);
+        renderVertex(2, face, vertexData.c, i, v, htmlV);
+        renderVertex(3, face, vertexData.d, i, v, htmlV);
+    }
+}
+
+
+/* Renders a vertex to the DOM
+---------------------------------------------------------------- */
+
+
+
 
 /* Returns A, B, C and D vertices of an element
 ---------------------------------------------------------------- */
 
-function computeVertexData (elem) {
+function computeVertexData(elem) {
     const w = elem.offsetWidth,
         h = elem.offsetHeight,
         v = {
-            a: { x: -w / 2, y: -h / 2, z: 0 },
-            b: { x:  w / 2, y: -h / 2, z: 0 },
-            c: { x:  w / 2, y:  h / 2, z: 0 },
-            d: { x: -w / 2, y:  h / 2, z: 0 }
+            a: {x: -w / 2, y: -h / 2, z: 0},
+            b: {x: w / 2, y: -h / 2, z: 0},
+            c: {x: w / 2, y: h / 2, z: 0},
+            d: {x: -w / 2, y: h / 2, z: 0}
         };
 
     while (elem.nodeType === 1) {
@@ -91,7 +93,7 @@ function computeVertexData (elem) {
 /* Returns the rotation and translation components of an element
 ---------------------------------------------------------------- */
 
-function getTransform (elem) {
+function getTransform(elem) {
     var computedStyle = getComputedStyle(elem, null),
         val = computedStyle.transform ||
             computedStyle.webkitTransform ||
@@ -133,8 +135,8 @@ function getTransform (elem) {
 /* Parses a matrix string and returns a 4x4 matrix
 ---------------------------------------------------------------- */
 
-function parseMatrix (matrixString) {
-    var c = matrixString.split(/\s*[(),]\s*/).slice(1,-1),
+function parseMatrix(matrixString) {
+    var c = matrixString.split(/\s*[(),]\s*/).slice(1, -1),
         matrix;
 
     if (c.length === 6) {
@@ -142,8 +144,8 @@ function parseMatrix (matrixString) {
         matrix = {
             m11: +c[0], m21: +c[2], m31: 0, m41: +c[4],
             m12: +c[1], m22: +c[3], m32: 0, m42: +c[5],
-            m13: 0,     m23: 0,     m33: 1, m43: 0,
-            m14: 0,     m24: 0,     m34: 0, m44: 1
+            m13: 0, m23: 0, m33: 1, m43: 0,
+            m14: 0, m24: 0, m34: 0, m44: 1
         };
     } else if (c.length === 16) {
         // matrix3d() (4x4)
@@ -169,7 +171,7 @@ function parseMatrix (matrixString) {
 /* Adds vector v2 to vector v1
 ---------------------------------------------------------------- */
 
-function addVectors (v1, v2) {
+function addVectors(v1, v2) {
     return {
         x: v1.x + v2.x,
         y: v1.y + v2.y,
@@ -181,7 +183,7 @@ function addVectors (v1, v2) {
 /* Rotates vector v1 around vector v2
 ---------------------------------------------------------------- */
 
-function rotateVector (v1, v2) {
+function rotateVector(v1, v2) {
     var x1 = v1.x,
         y1 = v1.y,
         z1 = v1.z,
@@ -201,17 +203,17 @@ function rotateVector (v1, v2) {
         y = cr * sp * cy + sr * cp * sy,
         z = cr * cp * sy - -sr * sp * -cy,
 
-        m0 = 1 - 2 * ( y * y + z * z ),
+        m0 = 1 - 2 * (y * y + z * z),
         m1 = 2 * (x * y + z * w),
         m2 = 2 * (x * z - y * w),
 
-        m4 = 2 * ( x * y - z * w ),
-        m5 = 1 - 2 * ( x * x + z * z ),
-        m6 = 2 * (z * y + x * w ),
+        m4 = 2 * (x * y - z * w),
+        m5 = 1 - 2 * (x * x + z * z),
+        m6 = 2 * (z * y + x * w),
 
-        m8 = 2 * ( x * z + y * w ),
-        m9 = 2 * ( y * z - x * w ),
-        m10 = 1 - 2 * ( x * x + y * y );
+        m8 = 2 * (x * z + y * w),
+        m9 = 2 * (y * z - x * w),
+        m10 = 1 - 2 * (x * x + y * y);
 
     return {
         x: x1 * m0 + y1 * m4 + z1 * m8,
@@ -228,7 +230,7 @@ function check(v) {
     f[3] = v[0] + v[1] + v[4] + v[5];
     f[4] = v[5] + v[1] + v[2] + v[6];
     f[5] = v[0] + v[3] + v[4] + v[7];
-    f.sort((a, b) => a-b);
+    f.sort((a, b) => a - b);
     for (let i = 1; i < 6; ++i) {
         if (f[i] === f[i - 1]) {
             return false;
@@ -240,7 +242,7 @@ function check(v) {
 
 function main(window, document) {
 // Select the faces
-    const faces =  Array.from(document.querySelectorAll(".face"));
+    const faces = Array.from(document.querySelectorAll(".face"));
 
     const input = document.querySelector(".input");
     const warn1 = document.querySelector(".warn1");
@@ -249,7 +251,7 @@ function main(window, document) {
     const arr0 = new Array(8).fill(0);
     const htmlV = new Array(8);
     renderFaces(faces, arr0, htmlV);
-    input.oninput = function() {
+    input.oninput = function () {
         warn1.classList.add("hidden");
         win.classList.add("hidden");
         error.classList.add("hidden");
@@ -258,7 +260,7 @@ function main(window, document) {
         let arrIndex = 0;
         for (let i = 0; i < str.length; ++i) {
             const candidate = str[i].charCodeAt() - '0'.charCodeAt();
-            if (candidate<=0 || candidate>8) {
+            if (candidate <= 0 || candidate > 8) {
                 continue;
             }
             if (arr.includes(candidate)) {
